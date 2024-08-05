f7ce98442f39e667e68f0c7f3fa87426bafbd5cc
Bruno-ADH <brunoadenhouessou@gmail.com>
2024-08-06 00:25:01 +0100

    Commit

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -4,38 +4,44 @@ import '../style/header.css';
 import { useToggle } from '../hooks/useToggle';
 
 const Header = () => {
-    const [state, toggle] = useToggle(false)
+  const [state, toggle] = useToggle(false)
+  const brandVisible = state ? {
+    visibility: "hidden",
+    pointerEvents: "none"
+  } : {
+    visibility: "visible",
+    pointerEvents: "visible"
+  }
 
-    return (
-      <Navbar bg="white" expand="lg" sticky='top' expanded={state}>
-        <Navbar.Brand href="#home" className="lg-visible ms-0">
+  return (
+    <Navbar bg="white" expand="lg" sticky='top' expanded={state}>
+      <Navbar.Brand href="#home" className="ms-0 my-3" style={brandVisible}>
         <img src="src/assets/icons/logo.svg" className="img-fluid" alt="Logo" style={{ width: '36px', height: '36px' }} />
-          </Navbar.Brand>
-        <Navbar.Toggle 
-          aria-controls="basic-navbar-nav" 
-          onClick={toggle}
-          className="btn-neutral-50 burger"
-        >
-          <img src={state ? "src/assets/icons/burgerClose-icon.svg" : "src/assets/icons/burger-icon.svg"} alt="Menu Toggle" />
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto fw-semibold">
-            <Nav.Link href="#home" onClick={state ? toggle() : undefined}>Home</Nav.Link>
-            <Nav.Link href="#create" onClick={state ? toggle() : undefined}>Top Sales</Nav.Link>
-            <Nav.Link href="#collections" onClick={state ? toggle() : undefined}>Collections</Nav.Link>
-            <Nav.Link href="#blog" onClick={state ? toggle() : undefined}>Our Blog</Nav.Link>
-            <Nav.Link href="#about" onClick={state ? toggle() : undefined}>About Us</Nav.Link>
-          </Nav>
-          <Nav className="ml-auto fw-semibold d-flex align-items-center">
-            <div className="d-flex align-items-center justify-content-md-evenly">
-            <Nav.Link href="#profile" className="ml-2" onClick={state ? toggle() : undefined}>Sign Up</Nav.Link>
+      </Navbar.Brand>
+      <Navbar.Toggle
+        aria-controls="basic-navbar-nav"
+        onClick={toggle}
+        className="btn-neutral-50 burger p-2 rounded-3"
+      >
+      </Navbar.Toggle>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mx-auto fw-semibold">
+          <Nav.Link href="#home" onClick={() => state ? toggle() : null}>Home</Nav.Link>
+          <Nav.Link href="#create" onClick={() => state ? toggle() : null}>Top Sales</Nav.Link>
+          <Nav.Link href="#collections" onClick={() => state ? toggle() : null}>Collections</Nav.Link>
+          <Nav.Link href="#blog" onClick={() => state ? toggle() : null}>Our Blog</Nav.Link>
+          <Nav.Link href="#about" onClick={() => state ? toggle() : null}>About Us</Nav.Link>
+        </Nav>
+        <Nav className="ml-auto fw-semibold d-flex align-items-center">
+          <div className="d-flex align-items-center justify-content-md-evenly">
+            <Nav.Link href="#profile" className="ml-2" onClick={() => state ? toggle() : null}>Sign Up</Nav.Link>
             <div className="vr ms-3 ms-lg-1 mx-3 align-self-center rounded divider"></div>
-            <Button variant="outline-dark" className="ml-2 btn-neutral-800 rounded-5 connect" onClick={state ? toggle() : undefined}>Connect Wallet</Button>
-            </div>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  };
+            <Button variant="outline-dark" className="ml-2 btn-neutral-800 rounded-5 connect" onClick={() => state ? toggle() : null}>Connect Wallet</Button>
+          </div>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
 export default Header;
