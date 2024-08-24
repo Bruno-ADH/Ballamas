46f2e226a2bee4d83ad722bd8f1c58d50496a24f
Bruno-ADH <brunoadenhouessou@gmail.com>
2024-08-24 18:25:48 +0100

    Commit

--- src/pages/Meta.jsx
+++ src/pages/Meta.jsx
@@ -12,18 +12,17 @@ function  Meta() {
         <link rel="apple-touch-icon" href="/android-chrome-192x192.png" sizes="192x192" />
         <link rel="apple-touch-icon" href="/android-chrome-512x512.png" sizes="512x512" />
         <meta property="og:title" content="BALLAMAS" />
-        <meta property="og:image" content={"https://ballamas.netlify.app" + featuredImage} />
+        <meta property="og:image" content="https://ballamas.netlify.app/ogProfil.jpg" />
         <meta property="og:type" content="article" />
         <meta property="og:site_name" content="BALLAMAS" />
         <meta property="og:locale" content="fr_FR" />
-        {/* <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" /> */}
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
         <title>BALLAMAS</title>
         <meta property="og:description" content="Discover Ballamas, your go-to online shop for quality products. Explore our wide selection and enjoy a seamless, fast, and secure shopping experience" />
         <meta property="og:url" content="https://ballamas.netlify.app/" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="description" content="Discover Ballamas, your go-to online shop for quality products. Explore our wide selection and enjoy a seamless, fast, and secure shopping experience" />
-        {/* <meta name="theme-color" content="#E5E5E5" /> */}
       </Helmet>
     </>
   );
