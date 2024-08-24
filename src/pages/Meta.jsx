e7b7f194d7e150752dad93ce052a5fb4f31a0585
Bruno-ADH <brunoadenhouessou@gmail.com>
2024-08-24 18:37:12 +0100

    Commit

--- src/pages/Meta.jsx
+++ src/pages/Meta.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import featuredImage from '/ogProfil.jpg'
 
 function  Meta() {
   return (
@@ -11,18 +10,20 @@ function  Meta() {
         <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
         <link rel="apple-touch-icon" href="/android-chrome-192x192.png" sizes="192x192" />
         <link rel="apple-touch-icon" href="/android-chrome-512x512.png" sizes="512x512" />
+
         <meta property="og:title" content="BALLAMAS" />
-        <meta property="og:image" content="https://ballamas.netlify.app/ogProfil.jpg" />
+        <meta property="og:image" content="https://ballamas.netlify.app/metaProfil.jpg" />
         <meta property="og:type" content="article" />
         <meta property="og:site_name" content="BALLAMAS" />
         <meta property="og:locale" content="fr_FR" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
-        <title>BALLAMAS</title>
         <meta property="og:description" content="Discover Ballamas, your go-to online shop for quality products. Explore our wide selection and enjoy a seamless, fast, and secure shopping experience" />
         <meta property="og:url" content="https://ballamas.netlify.app/" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="description" content="Discover Ballamas, your go-to online shop for quality products. Explore our wide selection and enjoy a seamless, fast, and secure shopping experience" />
+
+        <title>BALLAMAS</title>
       </Helmet>
     </>
   );
