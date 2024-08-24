ab8f3c7925aec637f11f8e08eeb1eabaafaa8b33
Bruno-ADH <brunoadenhouessou@gmail.com>
2024-08-24 18:52:14 +0100

    Commit

--- src/pages/Meta.jsx
+++ src/pages/Meta.jsx
@@ -11,19 +11,20 @@ function  Meta() {
         <link rel="apple-touch-icon" href="/android-chrome-192x192.png" sizes="192x192" />
         <link rel="apple-touch-icon" href="/android-chrome-512x512.png" sizes="512x512" />
 
+        <title>BALLAMAS</title>
+
         <meta property="og:title" content="BALLAMAS" />
-        <meta property="og:image" content="https://ballamas.netlify.app/metaProfil.jpg" />
-        <meta property="og:type" content="article" />
-        <meta property="og:site_name" content="BALLAMAS" />
-        <meta property="og:locale" content="fr_FR" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://ballamas.netlify.app/" />
+        <meta property="og:image" content="https://ballamas.netlify.app/metaProfil.jpg"/>
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="og:description" content="Discover Ballamas, your go-to online shop for quality products. Explore our wide selection and enjoy a seamless, fast, and secure shopping experience" />
-        <meta property="og:url" content="https://ballamas.netlify.app/" />
+        <meta property="og:site_name" content="BALLAMAS" />
+        <meta property="og:locale" content="fr_FR" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="description" content="Discover Ballamas, your go-to online shop for quality products. Explore our wide selection and enjoy a seamless, fast, and secure shopping experience" />
 
-        <title>BALLAMAS</title>
       </Helmet>
     </>
   );
