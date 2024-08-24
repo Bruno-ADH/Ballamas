21577e5cc049b08a1758ddd4dc2cad81b537f187
Bruno-ADH <brunoadenhouessou@gmail.com>
2024-08-24 17:23:56 +0100

    Commit

--- src/pages/Meta.jsx
+++ src/pages/Meta.jsx
@@ -11,7 +11,7 @@ function  Meta() {
         <link rel="apple-touch-icon" href="/android-chrome-192x192.png" sizes="192x192" />
         <link rel="apple-touch-icon" href="/android-chrome-512x512.png" sizes="512x512" />
         <meta property="og:title" content="BALLAMAS" />
-        <meta property="og:image" content="hero.jpg" />
+        <meta property="og:image" content="/hero.jpg" />
         <meta property="og:type" content="article" />
         <meta property="og:site_name" content="BALLAMAS" />
         <meta property="og:locale" content="fr_FR" />
